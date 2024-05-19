e86d505d169df0ffb45dae62b01c1b72c21fade1
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2024-05-19 12:51:34 +0200

    first working impl

---
+++ client.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+import random
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter as tkt
+
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(buffersize).decode("utf8")
+            print("Received message: ", msg)
+            msg_list.insert(tkt.END, msg)
+        except OSError as e:
+            break
+        
+def send(event=None):
+    msg = my_msg.get()
+    print("Sending message: ", msg)
+    my_msg.set("")
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+        window.quit()
+        
+def on_closing(event=None):
+    my_msg.set("{quit}")
+    send()
+
+def chat_gui():
+    print("Creating chat gui...")   
+    chat_frame = tkt.Frame(window)
+    global msg_list
+    scrollbar = tkt.Scrollbar(chat_frame)
+    scrollbar.pack(side=tkt.RIGHT, fill=tkt.Y)
+    msg_list = tkt.Listbox(chat_frame, height=15, width=80, yscrollcommand=scrollbar.set)
+    msg_list.pack(side=tkt.LEFT, fill=tkt.BOTH)
+    entry_field = tkt.Entry(window, textvariable=my_msg, width=50)
+    entry_field.bind("<Return>", func=send)
+    entry_field.pack()
+    send_button = tkt.Button(window, text="Send", command=send)
+    send_button.pack()
+    #loads the chat gui and hides the start page
+    start_frame.pack_forget()
+    chat_frame.pack()
+    
+def connect(server_ip, server_port, username):
+    #a new socket is created each time so that after a timeout there is no waiting time to try to reconnect
+    global client_socket
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.settimeout(1.0)
+    
+    #defaults if left blank
+    if server_ip == '':
+        server_ip = '127.0.0.1'
+    else:
+        server_ip = server_ip
+    if server_port == '':
+        server_port = 25565
+    else:
+        server_port = int(server_port)
+    if username == '':
+        
+        username = 'User' + str(random.randint(0, 1000))
+    else:
+        username = username
+        
+    print("Connecting to server at: ", server_ip, ":", server_port, "...")
+    try:
+        client_socket.connect((server_ip, server_port))
+        client_socket.settimeout(None)
+        print("Connected to server.")
+        chat_gui()
+        my_msg.set(username)
+        send()
+        receive_thread = Thread(target=receive)
+        receive_thread.start()
+    except Exception as e:
+        print("Error connecting to server: ", e)
+        return
+
+#init
+buffersize = 1024
+client_socket = None
+
+#tkt window
+window = tkt.Tk()
+server_ip = tkt.StringVar()
+server_port = tkt.StringVar()
+username = tkt.StringVar()
+my_msg = tkt.StringVar()
+#msg_list = tkt.Listbox(height=15, width=80)
+msg_list = None #global but not created yet
+window.title("Chat")
+window.protocol("WM_DELETE_WINDOW", on_closing)
+
+#start page frame
+start_frame = tkt.Frame(window, height=100, width=100, highlightthickness=40)
+ip_label = tkt.Label(start_frame, text="Server ip:")
+port_label = tkt.Label(start_frame, text="Server port:")
+name_label = tkt.Label(start_frame, text="Username:")
+ip_entry = tkt.Entry(start_frame, textvariable=server_ip)
+port_entry = tkt.Entry(start_frame, textvariable=server_port)
+name_entry = tkt.Entry(start_frame, textvariable=username)
+connect_button = tkt.Button(start_frame, text="Connect", command=lambda: connect(server_ip.get(), server_port.get(), username.get()))
+ip_label.pack()
+ip_entry.pack()
+port_label.pack()
+port_entry.pack()
+name_label.pack()
+name_entry.pack()
+connect_button.pack()
+start_frame.pack()
+tkt.mainloop()
--- server.py
+++ server.py
@@ -5,13 +5,15 @@ from threading import Thread
 def broadcast(msg, prefix='', exclusions=[]):
     for client_socket in clients:
         if client_socket not in exclusions:
-            client_socket.send(bytes(prefix+msg, "utf8"))
+            complete_msg = prefix + ": " + msg
+            print(f"Broadcasting message: {complete_msg}")
+            client_socket.send(bytes(complete_msg, "utf8"))
 
 def handle_client(client_socket):
     client_name = client_socket.recv(buffersize).decode("utf8")
     clients[client_socket] = client_name
 
-    welcome_message = f"Welcome {client_name}! If you want to leave the chat, type {quit} to exit."
+    welcome_message = "Welcome " +  client_name + r"! If you want to leave the chat, type  {quit}  to exit."
     client_socket.send(bytes(welcome_message, "utf8"))
 
     broadcast_message = f"{client_name} has joined the chat!"
@@ -19,10 +21,10 @@ def handle_client(client_socket):
 
     while True:
         msg = client_socket.recv(buffersize)
+        print(f"Received message from {client_name}: {msg}")
         if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, client_name)
+            broadcast(msg.decode("utf8"), prefix=client_name)
         else:
-            client_socket.send(bytes("Quitting from chat...", "utf8"))
             client_socket.close()
             del clients[client_socket]
             del addresses[client_socket]
@@ -31,11 +33,10 @@ def handle_client(client_socket):
 
 def main_loop():
     while True:
-        client_socket, client_ip = server.accept()
+        client_socket, client_ip = server_socket.accept()
         print(f"Connection from {client_ip}")
-        client_socket.send(bytes(f"Welcome to the server {client_ip}! Enter your name and press enter to join chat", "utf8"))
         addresses[client_socket] = client_ip
-        Thread(target=handle_client, args=(client_socket)).start()
+        Thread(target=handle_client, args=(client_socket,)).start()
 
 clients = {}
 addresses = {}
@@ -45,14 +46,13 @@ server_port = 25565
 server_address = (server_ip, server_port)
 buffersize = 1024
 
-server = socket(AF_INET, SOCK_STREAM)
-server.bind(server_address)
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind(server_address)
 
 if __name__ == "__main__":
-    server.listen(5)
+    server_socket.listen(5)
     print("Server is listening for connections...")
-    #server_thread = Thread(target=main_loop)
-    #server_thread.start()
-    #server_thread.join()
-    main_loop()
-    server.close()
+    server_thread = Thread(target=main_loop)
+    server_thread.start()
+    server_thread.join()
+    server_socket.close()
